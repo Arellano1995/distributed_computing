70908b3ddc3c8715cbc377bb1d7d66a7ec7db6f8
Arellano1995 <31712988+Arellano1995@users.noreply.github.com>
2019-10-03 17:54:12 -0700

    code comment

--- unidad_2/Example1.py
+++ unidad_2/Example1.py
@@ -1,3 +1,5 @@
+# This code shows how the Multiprocessing package allows our functions to work asynchronously.
+
 from multiprocessing import Process
 
 
@@ -5,7 +7,8 @@ def print_func(continent='Asia'):
     print('The name of continent is : ', continent)
 
 if __name__ == "__main__":  # confirms that the code is under main function
-    names = ['America', 'Europe', 'Africa']
+    names = ['America', 'Europe', 'Africa']     # The greater the number of calls we make to the function, the easier to observe asynchronous behavior. 
+    #names = ['ags', 'BC', 'BCS', 'Camp', 'Chis', 'Chih', 'CDM', 'Coah', 'Col', 'Dgo', 'Gto', 'Gro', 'Hgo', 'Jal']
     procs = []
     proc = Process(target=print_func)  # instantiating without any argument
     procs.append(proc)
@@ -17,7 +20,8 @@ if __name__ == "__main__":  # confirms that the code is under main function
         proc = Process(target=print_func, args=(name,))
         procs.append(proc)
         proc.start()
-
+    # In a synchronous system the list of names will be printed in the order in which they are listed.
+    # In this asynchronous system we can see that the names are printed in disorder, because the time of their process is different.
     # complete the processes
     for proc in procs:
 proc.join()
