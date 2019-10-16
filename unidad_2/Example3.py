cb2932206e4fdab07587eb7ee2dafc0437d81287
Arellano1995 <31712988+Arellano1995@users.noreply.github.com>
2019-10-15 17:33:21 -0700

    Code comment

--- unidad_2/Example3.py
+++ unidad_2/Example3.py
@@ -2,15 +2,20 @@ def print_name(prefix):
     print("Searching prefix:{}".format(prefix))
     try : 
         while True:
+                # Search matches
                 # yeild used to create coroutine
                 name = (yield)
                 if prefix in name:
                     print(name)
     except GeneratorExit:
-            print("Closing coroutine!!")
+                # finish the coroutine
+            print("Closing coroutine!!") 
  
-corou = print_name("Dear")
+corou = print_name("Lannister")
 corou.__next__()
-corou.send("James")
+#corou.send("James")
+#corou.send("Giovanni Lannister")
 corou.send("Dear James")
+corou.send("Paolo Sakatori")
+corou.send("Miguel Lannister")
 corou.close()
